b5b6a0af6614f66ca2a5370ec68b7fa429e8dd21
hamed <hamed.ma7@gmail.com>
2018-02-03 23:33:40 +0330

    added gson converter factory

--- app/src/main/java/com/hmomeni/retrofitsampleapp/MainActivity.java
+++ app/src/main/java/com/hmomeni/retrofitsampleapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 	Retrofit retrofit;
@@ -12,7 +13,11 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		retrofit = new Retrofit.Builder().build();
+
+		retrofit = new Retrofit.Builder()
+				.addConverterFactory(GsonConverterFactory.create())
+				.build();
+
+
 	}
 }
